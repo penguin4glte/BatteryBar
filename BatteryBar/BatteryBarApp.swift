33d8747fadaac37152432cf3b3cf84a69f2cd3e8
KazuyaMito <kazuya.mito@yappli.co.jp>
2026-04-18 14:11:08 +0900

    Initial Commit

---
+++ BatteryBar/BatteryBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatteryBarApp.swift
+//  BatteryBar
+//
+//  Created by kazuya.mito on 2026/04/18.
+//
+
+import SwiftUI
+
+@main
+struct BatteryBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BatteryBarTests/BatteryBarTests.swift
@@ -0,0 +1,17 @@
+//
+//  BatteryBarTests.swift
+//  BatteryBarTests
+//
+//  Created by kazuya.mito on 2026/04/18.
+//
+
+import Testing
+@testable import BatteryBar
+
+struct BatteryBarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
